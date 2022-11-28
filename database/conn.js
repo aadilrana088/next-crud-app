e68c4452853dfd6501ca26ffc3969bffad4ccad2
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-11-28 21:41:09 +0530

    MongoDB connected and created .env file

--- database/conn.js
+++ database/conn.js
@@ -1,44 +1,17 @@
-const MONGO_URI =
-    'mongodb+srv://admin:admin@nextjscrud.bkqnkmw.mongodb.net/?retryWrites=true&w=majority';
-
 import mongoose from 'mongoose';
 
-let cached = global.mongoose;
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
+const uri = process.env.MONGO_URI;
+
 const connectMongo = async () => {
-    // console.log('Outside');
-    // try {
-    //     const { connection } = await mongoose.connect(MONGO_URI);
-    //     if (connectMongo.readyState == 1) {
-    //         console.log('Database Connected');
-    //     }
-    // } catch (errors) {
-    //     Promise.reject(errors);
-    //     console.log('ab');
-    // }
-    if (cached.conn) {
-        return cached.conn;
-    }
+    try {
+        const { connection } = await mongoose.connect(uri);
 
-    if (!cached.promise) {
-        const opts = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            bufferCommands: false,
-            bufferMaxEntries: 0,
-            useFindAndModify: true,
-            useCreateIndex: true,
-        };
-
-        cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
-            console.log('abc');
-            return mongoose;
-        });
+        if (connection.readyState == 1) {
+            console.log('Database Connected');
+        }
+    } catch (errors) {
+        return Promise.reject(errors);
     }
-    cached.conn = await cached.promise;
-    return cached.conn;
 };
 
 export default connectMongo;
